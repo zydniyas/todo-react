7c35747a8ef29e327df8422a2a5f5a12f0d56874
niyas-mojgenie <niyas@mojgenie.com>
2024-02-07 18:05:14 +0530

    item delete done

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,43 @@ import ToDoBody from "./components/ToDoBody";
 export const TodoContext = createContext({});
 
 function App() {
-  const [note, setNote] = useState("");
-  const [todo, setTodo] = useState([]);
-  const addTodo = () => {
-    let currentTodo = { id: todo.length + 1, text: note };
-    setTodo([...todo, currentTodo]);
-    setNote("");
-  };
-  const updateTodo = (id, updatedValue) => {
-    let [editedTodo] = todo.filter((elem) => elem.id == id);
-    let tempTodo = [...todo];
-    tempTodo[id - 1] = { ...editedTodo, text: updatedValue };
-    setTodo(tempTodo);
-  };
-  return (
-    <>
-      <TodoContext.Provider
-        value={{ todo, setTodo, addTodo, note, setNote, updateTodo }}
-      >
-        <ToDoBody />
-        <InputFeid />
-      </TodoContext.Provider>
-    </>
-  );
+    const [note, setNote] = useState("");
+    const [todo, setTodo] = useState([]);
+    const addTodo = () => {
+        let currentTodo = { id: todo.length + 1, text: note };
+        setTodo([...todo, currentTodo]);
+        setNote("");
+    };
+    const updateTodo = (id, updatedValue) => {
+        let [editedTodo] = todo.filter((elem) => elem.id == id);
+        let tempTodo = [...todo];
+        tempTodo[id - 1] = { ...editedTodo, text: updatedValue };
+        setTodo(tempTodo);
+    };
+    const deleteTodo = (id) => {
+        let trashedTodo = todo.filter((elem) => elem.id == id);
+        let tempTodo = [...todo];
+        tempTodo.splice(id - 1, 1);
+        setTodo(tempTodo);
+    };
+    return (
+        <>
+            <TodoContext.Provider
+                value={{
+                    todo,
+                    setTodo,
+                    addTodo,
+                    note,
+                    setNote,
+                    updateTodo,
+                    deleteTodo,
+                }}
+            >
+                <ToDoBody />
+                <InputFeid />
+            </TodoContext.Provider>
+        </>
+    );
 }
 
 export default App;
